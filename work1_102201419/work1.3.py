64895ac42a7576f9abf160a78dcddd623880e0c6
Sonorry <951452955@qq.com>
2023-10-11 22:00:33 +0800

    refresh

--- work1_102201419/work1.3.py
+++ work1_102201419/work1.3.py
@@ -1,20 +1,7 @@
 # 输入⼀个字符串，判断字符串中是否含有"ol"这个⼦串，若有把所有的"ol"替换为"fzu"，最后把字符串倒序输出
 str_1 = input()
 str_2 = []
-flag = 0
-for i in str_1:
-    if flag == 0 and i == "o":
-       flag = flag + 1
-    elif flag == 1 and i == "l" :
-        flag = 0
-        str_2.append("f")
-        str_2.append("z")
-        str_2.append("u")
-    elif flag == 1 and i != "l" :
-        flag = 0
-        str_2.append("o")
-        str_2.append(i)
-    else:
-        str_2.append(i)
-str_2.reverse()
-print(''.join(str_2))
+str_replaced = "ol"
+if str_replaced in str_1 :
+    str_2 = str_1.replace(str_replaced,"fzu")
+print(str_2[::-1])
